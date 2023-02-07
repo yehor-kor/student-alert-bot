7ebde18c8109168846542fa9fcc929522a379085
Yehor Korablin <yegor_ka2004@ukr.net>
2023-02-08 00:11:50 +0200

    Config file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const telegramAPI = require("node-telegram-bot-api");
-const token = "6086564316:AAHx98sqw7rwWPLlXmRr2tKx_mJKK6RNUkI";
+const config = require("./config.json");
+const token = config.token;
 const bot = new telegramAPI(token, { polling: true });
 
 const start = () => {
@@ -17,7 +18,10 @@ const start = () => {
     { command: "/victory", description: "Play a funny sound 4" },
     { command: "/random", description: "Generate a random number from 1 to 6" },
     { command: "/grade", description: "Get a random grade" },
-    { command: "/setkeyword", description: "Set any keyword whatever you want" },
+    {
+      command: "/setkeyword",
+      description: "Set any keyword whatever you want",
+    },
     { command: "/sendsecret", description: "Send a message to developer" },
   ]);
 
@@ -27,100 +31,88 @@ const start = () => {
 
     console.log(msg);
 
-    if (text === keyword || text === "/alert" || text === "/alert@student_alert_bot") {
+    if (
+      text === keyword ||
+      text === "/alert" ||
+      text === "/alert@student_alert_bot"
+    ) {
       await bot.sendMessage(chatID, "🔴 ALERT, suka blyat");
       await bot.sendVoice(chatID, "./sounds/alert.ogg");
-    }
-
-    else if (text === "/completed" || text === "/completed@student_alert_bot") {
+    } else if (
+      text === "/completed" ||
+      text === "/completed@student_alert_bot"
+    ) {
       await bot.sendVoice(chatID, "./sounds/completed.ogg");
-    }
-    
-    else if (text === "/lightoff" || text === "/lightoff@student_alert_bot") {
+    } else if (text === "/lightoff" || text === "/lightoff@student_alert_bot") {
       await bot.sendVoice(chatID, "./sounds/light_off.ogg");
-    }
-    
-    else if (text === "/shiza" || text === "/shiza@student_alert_bot") {
+    } else if (text === "/shiza" || text === "/shiza@student_alert_bot") {
       await bot.sendVoice(chatID, "./sounds/shiza.ogg");
-    }
-    
-    else if (text === "/victory" || text === "/victory@student_alert_bot") {
+    } else if (text === "/victory" || text === "/victory@student_alert_bot") {
       await bot.sendVoice(chatID, "./sounds/victory.ogg");
-    }
-    
-    else if (text !== keyword && isSetting) {
+    } else if (text !== keyword && isSetting) {
       isSetting = false;
       keyword = text;
       await bot.sendMessage(
         chatID,
         "Okay, all right. A new keyword was added."
       );
-    }
-    
-    else if (text === keyword && isSetting) {
+    } else if (text === keyword && isSetting) {
       isSetting = false;
       await bot.sendMessage(
         chatID,
         "Oh shit, canceled. A new keyword and an old keyword are the same!"
       );
-    }
-
-    else if (text === "/start" || text === "/start@student_alert_bot" && isFirstTime) {
+    } else if (
+      text === "/start" ||
+      (text === "/start@student_alert_bot" && isFirstTime)
+    ) {
       isFirstTime = false;
       await bot.sendMessage(
         chatID,
         "Hello and welcome to Pereklichka bot!\nDeveloped by @yehor_kor"
       );
-    }
-
-    else if (text === "/start" || text === "/start@student_alert_bot" && !isFirstTime) {
+    } else if (
+      text === "/start" ||
+      (text === "/start@student_alert_bot" && !isFirstTime)
+    ) {
       await bot.sendMessage(chatID, "Hi! I am still here.");
-    }
-    
-    else if (text === "/random" || text === "/random@student_alert_bot") {
+    } else if (text === "/random" || text === "/random@student_alert_bot") {
       msg = await bot.sendDice(chatID);
       let randomNumber = msg.dice.value;
       await bot.sendMessage(chatID, randomNumber);
-    }
-    
-    else if (text === "/grade" || text === "/grade@student_alert_bot") {
+    } else if (text === "/grade" || text === "/grade@student_alert_bot") {
       let randomGrade = Math.floor(Math.random() * 40 + 60); // range [60; 100]
       await bot.sendMessage(
         chatID,
         `${msg.from.first_name} aka @${msg.from.username} has a ${randomGrade} grade!`
       );
-    }
-    
-    else if (text === "/setkeyword" || text === "/setkeyword@student_alert_bot") {
+    } else if (
+      text === "/setkeyword" ||
+      text === "/setkeyword@student_alert_bot"
+    ) {
       isSetting = true;
       await bot.sendMessage(
         chatID,
         "Please, send me any new keyword to turn on alarm."
       );
-    }
-    
-    else if (text === "/sendsecret" || text === "/sendsecret@student_alert_bot") {
+    } else if (
+      text === "/sendsecret" ||
+      text === "/sendsecret@student_alert_bot"
+    ) {
       isSecretMessage = true;
       await bot.sendMessage(
         chatID,
         "Please, send me any text message, I will deliver that to owner of this bot."
       );
-    }
-    
-    else if (isSecretMessage) {
+    } else if (isSecretMessage) {
       isSecretMessage = false;
-      await bot.sendMessage(
-        chatID,
-        "The text message has been sent."
-      );
+      await bot.sendMessage(chatID, "The text message has been sent.");
       await bot.sendMessage(
         // my tg chat`s code number
         724669680,
         `Secret message from ${msg.from.first_name} ${msg.from.last_name} aka @${msg.from.username}\n${text}`
       );
-    }
-    
-    else {
+    } else {
       await bot.sendMessage(chatID, "go nahui nubip");
     }
   });
